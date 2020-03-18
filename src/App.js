870b4111886985f751d78f31c616760e112d1890
Pawan Bola <pawan.bola2@gmail.com>
2020-03-18 14:37:07 +0530

    Headline propTypes test

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import "./app.scss";
 import Header from "./components/header";
 import Headline from "./components/headline";
 
+const tempArr = [
+  {
+    fName: "Joe",
+    lName: "Biden",
+    email: "joe.biden@us.com",
+    age: 24,
+    onlineStatus: true
+  }
+];
 function App() {
   return (
     <div className="App">
       <Header />
       <section className="main">
-        <Headline header="Posts" desc="description" />
+        <Headline header="Posts" desc="description" tempArr={tempArr} />
       </section>
     </div>
   );
--- src/components/headline/headline.test.js
+++ src/components/headline/headline.test.js
@@ -2,12 +2,32 @@ import React from "react";
 import { shallow } from "enzyme";
 
 import Headline from "./index";
-import { findByTestAttr } from "../../../utils/index";
+import { findByTestAttr, checkProps } from "../../../utils/index";
 
 const setUp = props => {
   return shallow(<Headline {...props} />);
 };
 describe("Headline Component", () => {
+  describe("checking PropTypes", () => {
+    it("should not throw a warning", () => {
+      const expectedProps = {
+        header: "Test Header",
+        desc: "test desc",
+        tempArr: [
+          {
+            fName: "test_fName",
+            lName: "test_lName",
+            email: "test@email.com",
+            age: 23,
+            onlineStatus: false
+          }
+        ]
+      };
+      const propsErr = checkProps(Headline, expectedProps);
+      expect(propsErr).toBe(undefined);
+    });
+  });
+
   describe("have props", () => {
     let wrapper;
 
